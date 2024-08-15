b6a3f27e84a97bb85788dfdfeb29465484807a20
Ritwik Katariya <58631634+ritkat@users.noreply.github.com>
2024-08-15 10:10:20 +0530

    Add files via upload
    Updated Hover Aviary to contain an eval function as well.

--- gym_pybullet_drones/envs/HoverAviary.py
+++ gym_pybullet_drones/envs/HoverAviary.py
@@ -12,6 +12,7 @@ class HoverAviary(BaseRLAviary):
                  drone_model: DroneModel=DroneModel.CF2X,
                  initial_xyzs=None,
                  initial_rpys=None,
+                 target_pos=np.array([1,1,1]),
                  physics: Physics=Physics.PYB,
                  pyb_freq: int = 240,
                  ctrl_freq: int = 30,
@@ -48,7 +49,7 @@ class HoverAviary(BaseRLAviary):
             The type of action space (1 or 3D; RPMS, thurst and torques, or waypoint with PID control)
 
         """
-        self.TARGET_POS = np.array([1,1,1])
+        self.TARGET_POS = target_pos
         self.EPISODE_LEN_SEC = 8
         super().__init__(drone_model=drone_model,
                          num_drones=1,
@@ -75,7 +76,7 @@ class HoverAviary(BaseRLAviary):
 
         """
         state = self._getDroneStateVector(0)
-        ret = 9 - np.linalg.norm(self.TARGET_POS-state[0:3])**4
+        ret = max(0, 9 - np.linalg.norm(self.TARGET_POS-state[0:3])**4)
         return ret
 
     ################################################################################
@@ -110,7 +111,6 @@ class HoverAviary(BaseRLAviary):
         if (abs(state[0]) > 1.5 or abs(state[1]) > 1.5 or state[2] > 2.0 # Truncate when the drone is too far away
              or abs(state[7]) > .4 or abs(state[8]) > .4 # Truncate when the drone is too tilted
         ):
-            print("toofar")
             return True
         if self.step_counter/self.PYB_FREQ > self.EPISODE_LEN_SEC:
             return True
@@ -131,3 +131,74 @@ class HoverAviary(BaseRLAviary):
 
         """
         return {"answer": 42} #### Calculated by the Deep Thought supercomputer in 7.5M years
+    
+class HoverAviary_eval(HoverAviary):
+
+    def __init__(self,
+                 drone_model: DroneModel=DroneModel.CF2X,
+                 initial_xyzs=None,
+                 initial_rpys=None,
+                 target_pos=np.array([1,1,1]),
+                 physics: Physics=Physics.PYB,
+                 pyb_freq: int = 240,
+                 ctrl_freq: int = 30,
+                 gui=False,
+                 record=True,
+                 obs: ObservationType=ObservationType.KIN,
+                 act: ActionType=ActionType.RPM
+                 ):
+        
+        self.TARGET_POS = target_pos
+        self.EPISODE_LEN_SEC = 8        
+        super().__init__(drone_model=drone_model,
+                            num_drones=1,
+                            initial_xyzs=initial_xyzs,
+                            initial_rpys=initial_rpys,
+                            physics=physics,
+                            pyb_freq=pyb_freq,
+                            ctrl_freq=ctrl_freq,
+                            gui=gui,
+                            record=record,
+                            obs=obs,
+                            act=act
+                            )
+
+    def _computeTruncated(self):
+        """Computes the current truncated value and prints if it is too far or too tilted
+
+        Returns
+        -------
+        bool
+            Whether the current episode timed out.
+
+        """
+        state = self._getDroneStateVector(0)
+        if (abs(state[0]) > 1.5 or abs(state[1]) > 1.5 or state[2] > 2.0): # Truncate when the drone is too far away
+            print("too far")
+            return True
+        elif(abs(state[7]) > .4 or abs(state[8]) > .4): # Truncate when the drone is too tilted
+            print("too tilted")
+            return True
+        if self.step_counter/self.PYB_FREQ > self.EPISODE_LEN_SEC:
+            return True
+        else:
+            return False
+        
+    def _computeTerminated(self):
+        """Computes the current done value.
+
+        Returns
+        -------
+        bool
+            Whether the current episode is done.
+
+        """
+        state = self._getDroneStateVector(0)
+        if np.linalg.norm(self.TARGET_POS-state[0:3]) < .0001:
+            print("too close")
+        # else:
+        return False
+        
+    ################################################################################
+
+    ################################################################################
